c41489997d638c0f59ccbfae713abf19ae64cfa4
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2023-03-29 15:49:17 -0700

    fix: css

--- src/components/NextBlock.js
+++ src/components/NextBlock.js
@@ -7,12 +7,12 @@ import { shapes } from '../utils'
 // Draws the "next" block view showing the next block to drop
 export default function NextBlock(props) {
   const nextShape = useSelector((state) => state.nextShape)
-  const block = shapes[nextShape][0] // Get the first rotation
+  const box = shapes[nextShape][0] // Get the first rotation
   // Map the block to the grid
-  const grid = block.map((rowArray, row) => {
+  const grid = box.map((rowArray, row) => {
     return rowArray.map((square, col) => {
     const color = square ? nextShape : 0
-      return <GridSquare key={`${row}${col}`} color={color} />
+      return <GridSquare key={`${row}${col}`} color={square === 0 ? 0 : nextShape} />
     })
   })
   
