51a16038807a9befce835511d41341928bbe17ba
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2023-04-05 12:12:14 -0700

    add: electron

--- public/electron.js
+++ public/electron.js
@@ -1,40 +1,74 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
 
-const path = require('path');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
 
-let mainWindow;
+let installExtension, REACT_DEVELOPER_TOOLS; // NEW!
+
+if (isDev) {
+  const devTools = require("electron-devtools-installer");
+  installExtension = devTools.default;
+  REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS;
+} // NEW!
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+if (require("electron-squirrel-startup")) {
+  app.quit();
+} // NEW!
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 900, 
-    height: 680,
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 600,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
-  }
-});
- mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+    }
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  // Open the DevTools.
   if (isDev) {
-    // Open the DevTools.
-    // BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-    // Open Chrome web inspector for debug and development
-    // mainWindow.webContents.openDevTools();
+    win.webContents.openDevTools({ mode: "detach" });
   }
-  mainWindow.on('closed', () => mainWindow = null);
 }
 
-app.on('ready', createWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  if (isDev) {
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(error => console.log(`An error occurred: , ${error}`));
+  }
+}); // UPDATED!
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  if (mainWindow === null) {
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them
